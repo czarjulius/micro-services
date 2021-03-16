ab00d7b1d1e213d308df1f332cf4b54548f8ce30
czarjulius <juliusczar.jc@gmail.com>
2021-03-16 12:01:02 +0100

    user model

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-
+import {User} from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -22,15 +23,20 @@ async(req: Request, res: Response)=>{
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array())
   }
-  // const {email, password} = req.body;
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
+  const {email, password} = req.body;
 
+  const existingUser = await User.findOne({email})
 
-  res.send({})
-  
-  // res.send('Hi there!')
+  if (existingUser) {
+    throw new BadRequestError('Email in use')
+  }
+
+  const user = User.build({email, password})
+  await user.save()
+
+  res.status(201).send(user)
+   
 })
 
 export { router as signupRouter };
