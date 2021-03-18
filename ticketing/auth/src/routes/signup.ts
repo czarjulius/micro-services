1d014f2555503912e1f5c98390716726c2ff6756
czarjulius <juliusczar.jc@gmail.com>
2021-03-18 14:26:06 +0100

    signin signout currentuser and sighup

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken'
+
+import { validateRequest } from '../middlewares/validate-request';
 import {User} from '../models/user'
-import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -16,13 +17,8 @@ router.post('/api/users/signup', [
     .isLength({min: 4, max: 20})
     .withMessage('Password must be between 4 and 20 characters')
 ], 
+validateRequest,
 async(req: Request, res: Response)=>{
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
-  }
 
   const {email, password} = req.body;
 
@@ -34,6 +30,13 @@ async(req: Request, res: Response)=>{
 
   const user = User.build({email, password})
   await user.save()
+
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, process.env.JWT_KEY!)
+
+  req.session = {jwt: userJwt}
 
   res.status(201).send(user)
    
